4832e36bb55693ca7c714a5670d31ef5727a19e9
maria eduarda <146000904+madumarrafon@users.noreply.github.com>
2025-05-06 11:36:59 -0300

    kidbengala

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll (".botao");
+
+for (let i = 0; i <botoes,length; i++){
+    botoes (1).onclick = function (){
+        for (let j = 0; j <botoes.lenght; j++){
+            botoes(j).classList.remove("ativo");
+        }
+
+        botoes(i).classList.add("ativo");
+    }
+}
