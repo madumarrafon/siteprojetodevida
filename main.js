2d0e384afd8d68055ec18c16b1f660d0129560e0
maria eduarda <146000904+madumarrafon@users.noreply.github.com>
2025-06-03 11:09:15 -0300

    jgkr

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i <botoes,length; i++){
-    botoes [1].onclick = function (){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for (let j = 0; j <botoes.lenght; j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -14,15 +14,14 @@ for (let i = 0; i <botoes,length; i++){
     }
 }
 
-const contadores = document.querySelectorAlll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContext = tempoObjetivo1 - tempoAtual;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-
-
-contadores[0].textContext = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -35,6 +34,22 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
-    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    if (tempoFinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
 }
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
